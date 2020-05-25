8dedeb1343f3969c73e4f633144cf5983cc6d10b
Cameron McKenzie <38986973+cameronmcnz@users.noreply.github.com>
2020-05-25 15:38:24 -0400

    Create Link.java

---
+++ com/mcnz/wordpress/Link.java
@@ -0,0 +1,169 @@
+package com.mcnz.wordpress;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+import java.math.BigInteger;
+
+
+/**
+ * The persistent class for the wp_links database table.
+ * 
+ */
+@Entity
+@Table(name="wp_links")
+@NamedQuery(name="Link.findAll", query="SELECT l FROM Link l")
+public class Link implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="link_id")
+	private String linkId;
+
+	@Column(name="link_description")
+	private String linkDescription;
+
+	@Column(name="link_image")
+	private String linkImage;
+
+	@Column(name="link_name")
+	private String linkName;
+
+	@Lob
+	@Column(name="link_notes")
+	private String linkNotes;
+
+	@Column(name="link_owner")
+	private BigInteger linkOwner;
+
+	@Column(name="link_rating")
+	private int linkRating;
+
+	@Column(name="link_rel")
+	private String linkRel;
+
+	@Column(name="link_rss")
+	private String linkRss;
+
+	@Column(name="link_target")
+	private String linkTarget;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="link_updated")
+	private Date linkUpdated;
+
+	@Column(name="link_url")
+	private String linkUrl;
+
+	@Column(name="link_visible")
+	private String linkVisible;
+
+	public Link() {
+	}
+
+	public String getLinkId() {
+		return this.linkId;
+	}
+
+	public void setLinkId(String linkId) {
+		this.linkId = linkId;
+	}
+
+	public String getLinkDescription() {
+		return this.linkDescription;
+	}
+
+	public void setLinkDescription(String linkDescription) {
+		this.linkDescription = linkDescription;
+	}
+
+	public String getLinkImage() {
+		return this.linkImage;
+	}
+
+	public void setLinkImage(String linkImage) {
+		this.linkImage = linkImage;
+	}
+
+	public String getLinkName() {
+		return this.linkName;
+	}
+
+	public void setLinkName(String linkName) {
+		this.linkName = linkName;
+	}
+
+	public String getLinkNotes() {
+		return this.linkNotes;
+	}
+
+	public void setLinkNotes(String linkNotes) {
+		this.linkNotes = linkNotes;
+	}
+
+	public BigInteger getLinkOwner() {
+		return this.linkOwner;
+	}
+
+	public void setLinkOwner(BigInteger linkOwner) {
+		this.linkOwner = linkOwner;
+	}
+
+	public int getLinkRating() {
+		return this.linkRating;
+	}
+
+	public void setLinkRating(int linkRating) {
+		this.linkRating = linkRating;
+	}
+
+	public String getLinkRel() {
+		return this.linkRel;
+	}
+
+	public void setLinkRel(String linkRel) {
+		this.linkRel = linkRel;
+	}
+
+	public String getLinkRss() {
+		return this.linkRss;
+	}
+
+	public void setLinkRss(String linkRss) {
+		this.linkRss = linkRss;
+	}
+
+	public String getLinkTarget() {
+		return this.linkTarget;
+	}
+
+	public void setLinkTarget(String linkTarget) {
+		this.linkTarget = linkTarget;
+	}
+
+	public Date getLinkUpdated() {
+		return this.linkUpdated;
+	}
+
+	public void setLinkUpdated(Date linkUpdated) {
+		this.linkUpdated = linkUpdated;
+	}
+
+	public String getLinkUrl() {
+		return this.linkUrl;
+	}
+
+	public void setLinkUrl(String linkUrl) {
+		this.linkUrl = linkUrl;
+	}
+
+	public String getLinkVisible() {
+		return this.linkVisible;
+	}
+
+	public void setLinkVisible(String linkVisible) {
+		this.linkVisible = linkVisible;
+	}
+
+}
